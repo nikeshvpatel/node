ef89c54da4eeada3daa0d28d9c8713f303107376
TBnikkumar <nikesh.kumar@travelbrands.com>
2023-02-10 16:20:19 -0500

    adding full MVC with creating data file

--- controllers/products.controller.js
+++ controllers/products.controller.js
@@ -4,23 +4,24 @@ function getAddProduct(req, res) {
     return res.status(200).render('add-products', {pageTitle: 'Add Product', path: '/admin/add-product', formsCSS: true, productCSS: true, activeAddProduct: true});
 }
 
-function postAddProduct(req, res) {
+async function postAddProduct(req, res) {
     // products.push({...req.body});
     const product = new Product(req.body.title);
-    product.save();
+    await product.save();
     res.redirect('/');
 }
 
-function getProducts(req, res) {
-    const products = Product.fetchAll();
-    res.status(200).render('shop', {
-        prods: products,
-        pageTitle: 'Shop',
-        path: '/',
-        hasProducts: products.length > 0,
-        activeShop: true,
-        productCSS: true
-    })
+async function getProducts(req, res) {
+    let products = await Product.fetchAll();
+    res.status(200)
+        .render('shop', {
+            prods: products,
+            pageTitle: 'Shop',
+            path: '/',
+            hasProducts: products.length > 0,
+            activeShop: true,
+            productCSS: true
+        })
 }
 
 module.exports = {getAddProduct, postAddProduct, getProducts};
--- models/product.model.js
+++ models/product.model.js
@@ -1,18 +1,40 @@
-const products = [];
+const fs = require('fs');
+const path = require('path');
+
+const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json')
+
 
 class Product {
+
     constructor(title) {
         this.title = title;
     }
 
+
     save() {
-        products.push(this);
+        fs.readFile(p, (err, data) => {
+            let products = [];
+            if (!err) {
+                products = JSON.parse(data);
+            }
+            products.push(this);
+            fs.writeFile(p, JSON.stringify(products), err => {
+                console.log(err);
+            });
+        });
+
     }
 
     static fetchAll() {
-        return products;
+        return new Promise((resolve) => {
+            fs.readFile(p, (err, data) => {
+                if (err) {
+                    return resolve([]);
+                }
+                resolve(JSON.parse(data));
+            })
+        })
     }
-
 }
 
 module.exports = {Product}
